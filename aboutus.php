366fdb9c1e5cac73bcb60b064f2e289330c07d1f
mark kinai <mark1cyfred1@gmail.com>
2023-08-23 20:39:25 +0300

    about

---
+++ aboutus.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+      <title>Cypressmark</title>
+    
+      <!-- Favicons -->
+      <link href="assets/img/logo11.jpg" rel="icon">
+      
+
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+      <!-- Vendor CSS Files -->
+      
+      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      
+    
+
+      <!-- Template Main CSS File -->
+      <link href="assets/css/main.css" rel="stylesheet">
+      <link href="assets/css/shapes.css" rel="stylesheet">
+     
+      <link href="assets/css/belo.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+      
+      
+    </head>
+
+  <body>
+      
+      <!-- ======= Header ======= -->
+      <header id="header" class="header fixed-top">
+        <div id="shapes-container"></div>
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+          <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+            <!-- Logo before scrolling -->
+            <div style="width: 180px; margin-left: -50px; " id="headerLogoBeforeScroll" class="logo">
+              <span>CypressMark</span>
+                
+            </div>  
+        </div>
+                <nav id="navbar" class="navbar">
+            <ul>
+              <li><a class="nav-link scrollto active" href="index.html">Home</a></li>
+              <li><a class="nav-link scrollto" href="aboutus.php">About</a></li>
+              <li><a class="nav-link scrollto" href="#services">Services</a></li>
+              <li><a class="nav-link scrollto" href="#portfolio">Products</a></li>
+              <li><a class="nav-link scrollto" href="#team">Domain </a></li>
+              <li><a class="nav-link scrollto" href="#blog">Blog</a>   </li>
+              
+              <li><a class="nav-link scrollto" href="#pricing">Web Design</a></li>
+              <li><a class="nav-link" href="#graphic design">Graphic Design</a></li>
+
+              <li class="dropdown megamenu">
+                <a href="#" class="dropdown-toggle"><span>Resources</span> <i class="bi bi-chevron-down"></i></a>
+                <ul class="megamenu-content">
+                    <li>
+                        <a href="#"><strong style="color: #007700;">Blog</strong></a>
+                        <a href="#">News & Trends</a>
+                        <a href="#">Education</a>
+                    </li>
+                    <li>
+                        <a href="#"><strong style="color: #007700;">Support</strong></a>
+                        <a href="#">Similarweb Academy</a>
+                        <a href="#">Webinars</a>
+                    </li>
+                    <li>
+                        <a href="#"><strong style="color: #007700;">Company</strong></a>
+                        <a href="#">Careers</a>
+                        <a href="#">Investor Relations</a>
+                    </li>
+                </ul>
+            </li>
+                            
+            <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
+
+        </div>
+      </header><!-- End Header -->
+      <body>
+          <!-- Add your header/navigation section here -->
+        
+          <!-- About Us Section -->
+          <section id="about" class="about">
+            <div class="container" data-aos="fade-up">
+              <div class="row gx-0">
+                <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                  <div class="content">
+                    <h3>About Us</h3>
+                    <h2>We Stand Out as a Reliable Ally</h2>
+                    <p>
+                      CypressMark is a dynamic and cutting-edge marketing company that collaborates with businesses to develop successful marketing plans and initiatives. Our committed team of professionals provides a wide range of services, including site design, content creation, branding, and digital marketing solutions.
+                    </p>
+                    <div class="text-center text-lg-start">
+                      <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
+                        <span>Read More</span>
+                        <i class="bi bi-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
+                  <img src="assets/img/banner.jpeg" class="img-fluid" alt="">
+                </div>
+              </div>
+            </div>
+          </section>
+        
+          <footer id="footer" class="footer">
+                    <div class="footer-top">
+                       <div class="container">
+                         <div class="row gy-4">
+                           <div class="col-lg-5 col-md-12 footer-info">
+                             <a href="index.html" class="logo d-flex align-items-center">
+                               <img src="assets/img/logo1.png" alt="">
+                               <span>Cypressmark</span>
+                             </a>
+                             <p>We stand out as a reliable ally in attaining marketing success and business growth thanks to our focused on customers methodology, data-driven insights, and creative prowess.</p>
+                             <div class="social-links mt-3">
+                               
+                               <a href="https://www.facebook.com/mark.kinai.1/" class="facebook"><i class="bi bi-facebook"></i></a>
+                              
+                               <a href="https://www.linkedin.com/in/mark-kinai-2948b3275/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                             </div>
+                           </div>
+                           <div class="col-lg-2 col-6 footer-links">
+                             <h4>Useful Links</h4>
+                             <ul>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="aboutus.html">About us</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                             </ul>
+                           </div>
+                           <div class="col-lg-2 col-6 footer-links">
+                             <h4>Our Services</h4>
+                             <ul>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
+                               <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                             </ul>
+                           </div>
+                           <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                             <h4>Contact Us</h4>
+                             <p>
+                             Fedha <br>
+                               Nairobi, NY 535022<br>
+                               Kenya <br><br>
+                               <strong>Phone:</strong> +1254 111 893 789<br>
+                               <strong>Email:</strong> info@cypressmark.com<br>
+                             </p>
+                           </div>
+                         </div>
+                       </div>
+                     </div>
+                     </div>
+                   </footer><!-- End Footer -->
+                   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+                   <!-- Vendor JS Files -->
+               
+                   <script src="assets/vendor/aos/aos.js"></script>
+                 
+                   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+                   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+                   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+                   <script src="assets/vendor/php-email-form/validate.js"></script>
+                   
+                   <script src="assets/js/main.js"></script>
+                   <script>
+                   function loadShapesContent()
+                    {
+                     fetch('shapes.html') 
+                         .then(response => response.text())
+                         .then(data => {
+                             const shapesContainer = document.getElementById('shapes-container');
+                             shapesContainer.innerHTML = data;
+                         })
+                         .catch(error => {
+                             console.error('Error loading shapes content:', error);
+                         });
+                 }
+               
+                 loadShapesContent();</script>
+        </body>
+          </html>
