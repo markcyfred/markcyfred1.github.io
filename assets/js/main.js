0ffb057575f962a38040f7eca86d5df1d4aab911
markcyfred <dcsnrb594822@spu.ac.ke>
2024-03-25 09:01:57 +0300

    jjf

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,17 @@
 //     event.stopPropagation();
 // }, true);
 
-//  function loadHTMLPage(url, containerId) {
-//     var container = document.getElementById(containerId);
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             container.innerHTML = this.responseText;
-//         }
-//     };
-//     xhttp.open("GET", url, true);
-//     xhttp.send();
-// }
+ function loadHTMLPage(url, containerId) {
+    var container = document.getElementById(containerId);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            container.innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
 
 loadHTMLPage('shapes.html', 'shapes-container');
 
